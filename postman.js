f4cccef665ee03f30c83a3f7273c2f0bf1a6505f
Kimi Riecs <kimi@example.com>
2022-01-16 10:17:40 +0200

    Update postman.js

--- postman.js
+++ postman.js
@@ -1,24 +1,29 @@
 // Add "domain.com" to Cookie->WhiteList
 // to allow access to the domain's cookies from scripts
 
-pm.sendRequest({
-    url:'domain.com/sanctum/csrf-cookie',
-    method: 'GET',
-    header: {
-        'Accept': 'application/json'
-    }
-  },
-  function (error, response) {
-    if(!error){
-        const cookieJar = pm.cookies.jar();
+const backend = pm.environment.get("host")
+const sanctumCookie = pm.environment.get("sanctum_csrf")
 
-        cookieJar.get('domain.com', "XSRF-TOKEN", function(error, cookie){
-                pm.environment.set("x-xsrf", cookie)
-            }
-        );
+if(pm.request.method === 'POST'){
+    pm.sendRequest({
+        url: backend + '/' + sanctumCookie,
+        method: 'GET',
+        header: {
+            'Accept': 'application/json'
+        }
+    },
+    function (error, response) {
+        if(!error){
+            const cookieJar = pm.cookies.jar();
+
+            cookieJar.get(backend, "XSRF-TOKEN", function(error, cookie){
+                    pm.environment.set("x-xsrf", cookie)
+                }
+            );
+        }
     }
-  }
-)
+    )
+}
 
 
 // const cookieJar = pm.cookies.jar();
